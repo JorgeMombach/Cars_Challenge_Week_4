5962ef79abadf23a9193ad4af3c7204f47a7295b
JamaykonCZ <jorge.mombach@gmail.com>
2023-07-12 14:57:19 -0300

    Added Car

---
+++ car/src/main/java/br/com/jorge/car/entity/Car.java
@@ -0,0 +1,87 @@
+package br.com.jorge.car.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CAR_TB")
+public class Car {
+
+    @Id
+    @SequenceGenerator(
+            name = "car_sequence",
+            sequenceName = "car_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "car_sequence"
+    )
+
+    private Long idChassi;
+    private String name;
+    private String brand;
+    private String color;
+    private String fabricationYear;
+
+    public Car() {
+    }
+
+    public Car(Long idChassi, String name, String brand, String color, String fabricationYear) {
+        this.idChassi = idChassi;
+        this.name = name;
+        this.brand = brand;
+        this.color = color;
+        this.fabricationYear = fabricationYear;
+    }
+
+    public Long getIdChassi() {
+        return idChassi;
+    }
+
+    public void setIdChassi(Long idChassi) {
+        this.idChassi = idChassi;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFabricationYear() {
+        return fabricationYear;
+    }
+
+    public void setFabricationYear(String fabricationYear) {
+        this.fabricationYear = fabricationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "idChassi=" + idChassi +
+                ", name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                ", fabricationYear='" + fabricationYear + '\'' +
+                '}';
+    }
+}
--- car/src/main/java/br/com/jorge/car/repository/CarRepository.java
+++ car/src/main/java/br/com/jorge/car/repository/CarRepository.java
@@ -1,5 +1,6 @@
 package br.com.jorge.car.repository;
 
+import br.com.jorge.car.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
